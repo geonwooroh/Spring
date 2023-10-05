28e446c211e674467bc40bd01905b50989bba8da
geonwoo Roh <120004306+geonwooroh@users.noreply.github.com>
2023-10-05 16:14:47 +0900

    Add files via upload

---
+++ mylogin/src/main/java/com/codingbox/mylogin/domain/item/Item.java
@@ -0,0 +1,47 @@
+package com.codingbox.mylogin.domain.item;
+
+import java.util.List;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class Item {
+	private Long id;
+	private String itemName;
+	private Integer price;		// null일 수 있다.
+	private Integer	quantity;	// null일 수 있다.
+	
+	//기능 추가
+	private Boolean open; // 판매여부
+	private List<String> regions; //등록지역
+	private ItemType itemType;	  //상품종류
+	private String deliveryCode; //배송방식
+	
+	public Item() {
+		super();
+	}
+	
+	public Item(String itemName, Integer price, Integer quantity) {
+		super();
+		this.itemName = itemName;
+		this.price = price;
+		this.quantity = quantity;
+	}
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- mylogin/src/main/java/com/codingbox/mylogin/domain/login/LoginController.java
+++ mylogin/src/main/java/com/codingbox/mylogin/domain/login/LoginController.java
@@ -5,6 +5,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.codingbox.mylogin.domain.member.Member;
@@ -48,7 +49,7 @@ public class LoginController {
 			
 	}
 	
-	@PostMapping("/login")
+	//@PostMapping("/login")
 	public String loginV2( @ModelAttribute LoginForm form,
 			Model model, RedirectAttributes redirectAttrs,
 			HttpServletRequest request) {
@@ -72,6 +73,30 @@ public class LoginController {
 			
 	}
 	
+	@PostMapping("/login")
+	public String loginV3( @ModelAttribute LoginForm form,
+			Model model, RedirectAttributes redirectAttrs,
+			HttpServletRequest request, @RequestParam(defaultValue = "/")String redirectURL) {
+		
+		Member loginMember
+			= loginService.login(form.getLoginId(), form.getPassword());
+		
+		if( loginMember == null ) {
+			// 로그인 실패시
+			model.addAttribute("msg", "로그인 실패");
+			return "login/loginForm";
+		} else {
+			// 로그인 성공시
+			// 세션이 있으면 있는 세션 반환, 없으면 신규 세션 생성
+			HttpSession session = request.getSession();
+			// 새션에 로그인 회원 정보 보관
+			session.setAttribute(SessionConst.LOGIN_MEMBER, loginMember);
+			redirectAttrs.addFlashAttribute("msg", "로그인성공");
+			return "redirect:" + redirectURL;
+		}
+			
+	}
+	
 	//@PostMapping("/logout")
 	public String logout(HttpServletResponse response) {
 		// 쿠키 만료
@@ -81,13 +106,14 @@ public class LoginController {
 	
 	@PostMapping("/logout")
 	public String logoutV2(HttpServletRequest request) {
-		// 세션을 삭제
+		// 센션을 삭제
 		HttpSession session = request.getSession(false);
 		if( session != null ) {
 			session.invalidate();
 		}
 		return "redirect:/";
 	}
+	
 	
 	// 쿠키 만료
 	private void expireCookie( HttpServletResponse response,
